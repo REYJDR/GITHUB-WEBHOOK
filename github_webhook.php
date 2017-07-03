8efa0f6f21a2935a434ca27f2efd26268a2175f6
Reinaldo Daou <reinaldo.daou@gmail.com>
2017-07-03 17:27:40 -0500

    INITIAL COMMIT

---
+++ github_webhook.php
@@ -0,0 +1,25 @@
+<?php
+
+$ficheros  = scandir('/home/daoutel/public_html/');
+
+foreach($ficheros as $files){
+
+        $find_aci = scandir($files);
+
+        foreach($find_aci as $folder){
+         if($folder == 'ACIWEB'){
+
+        $exce =   exec('cd /home/daoutel/public_html/'.$files.'/'.$folder.' &&  git pull');
+        file_put_contents("github-event.log","**** ".date("Y-m-d h:i:sa").' **** '.$files.'/'.$folder." ****\n--INI--\n ".$exce."\n--END--\n",FILE_APPEND);
+
+        echo date("Y-m-d h:i:sa").' -  '.$files.'/'.$folder.'<br>  '.$exce.'<br><br> ****** <br>';
+          }
+        }
+
+
+}
+
+/*
+echo $array =  $HTTP_RAW_POST_DATA;
+*/
+?>
